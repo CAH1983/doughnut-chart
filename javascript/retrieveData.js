9f8ea071a069cc0ddb0eaaec7dc1d5bcb7a33aea
CAH1983 <carolineho1983@yahoo.fr>
2019-02-16 19:43:15 +0000

    added CSS and styling

--- javascript/retrieveData.js
+++ javascript/retrieveData.js
@@ -2,15 +2,13 @@ const getPizzasBtn = document.getElementById('getPizzas');
 const pizzasDisplay = document.getElementById('pizzasDisplay');
 const imgArray = [
   'https://cdn-images-1.medium.com/max/1600/1*Wlx63hI3q70lKaGtVvH-ZA.jpeg',
-  'https: //scontent-lht6-1.cdninstagram.com/vp/1e2f35e87ddcf6cf620644ce875efaba/5CD08642/t51.2885-15/e35/49579484_417943578746850_6056746193828834487_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com',
-  'https: //scontent-lht6-1.cdninstagram.com/vp/9155de84cbfbf029358b4632e49d6e47/5CC2A75C/t51.2885-15/e35/47693617_780997805598486_8832491888258135323_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com',
-  'https: //scontent-lht6-1.cdninstagram.com/vp/09a30c8034b15469e5feda41b1a66207/5CC76452/t51.2885-15/e35/49933800_278560039502221_3707768412519390180_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com',
-  'https: //scontent-lht6-1.cdninstagram.com/vp/06c8a405d4da45722a71a23f06182dd3/5CC6E04B/t51.2885-15/fr/e15/s1080x1080/47692966_344489469473010_5988587681001039587_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com',
+  'https://s3-media2.fl.yelpcdn.com/bphoto/ejfH5dBk4pm_PI_TnLaxWQ/ls.jpg',
+  'https://www.tasteofhome.com/wp-content/uploads/2018/04/Whole-Wheat-Veggie-Pizza_EXPS_HCKA19_12558_C10_13_5b-696x696.jpg',
+  'https://www.healthymummy.com/wp-content/uploads/2016/08/Cauliflower-Pizza-Crust.jpg',
+  'http://storage-cube.quebecormedia.com/v1/cl_prod/canadian_living/f6b9deb0bca458c301eb1825a264364acba975a2/pizza-margherita.jpg',
+  'https://img.sunset02.com/sites/default/files/image/recipes/su/10/02/pizzetta-margherita-pizza-su-x.jpg'
+
 ]
-
-
-
-function getPhotos() {
 
   imgArray.forEach(url => {
     let newLi = document.createElement('LI');
@@ -19,6 +17,4 @@ function getPhotos() {
     pizzasDisplay.appendChild(image);
   })
 
-}
 
-getPizzasBtn.addEventListener('click', getPhotos);
