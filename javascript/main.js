bf6051cab909bfe031ff1ef9dc17da14f3dc4bc3
CAH1983 <carolineho1983@yahoo.fr>
2019-02-16 16:35:15 +0000

    looping photos

---
+++ javascript/main.js
@@ -0,0 +1,63 @@
+let mainChart = document.getElementById('mainChart').getContext('2d');
+
+
+// global options for fonts
+
+Chart.defaults.global.defaultFontFamily = 'Lato';
+Chart.defaults.global.defaultFontSize =  14;
+Chart.defaults.global.defaultFontColor =  'grey';
+
+
+let massPopChart = new Chart(mainChart, {
+  type: 'doughnut', //type of chart
+  data: {
+    datasets: [{
+      label: 'Population',
+      data: [
+        617594,
+        181045,
+        153060,
+        106519,
+        105162,
+        95072
+      ],
+      // custom colors and hover effect on the chart
+      backgroundColor:[
+
+        'yellow',
+        'orange',
+        'blue',
+        'pink',
+        'brown',
+        'violet'
+      ],
+      borderWidth: 2,
+      borderColor: '#777',
+      hoverBorderWidth: 3,
+      hoverBorderColor: 'white'
+    }]
+  },
+  options: {
+    title:{
+      display:true,
+      text: 'Best pizzas in town',
+      fontSize: 25
+    },
+
+    legend:{
+      position: 'left',
+      labels:{
+        fontColor:'black'
+      }
+    },
+
+    layout:{
+      padding:{
+        left: 50,
+        right: 50,
+        top: 50,
+        bottom: 50
+      },
+    }
+  }
+});
---
+++ javascript/retrieveData.js
@@ -0,0 +1,40 @@
+const getPizzasBtn = document.getElementById('getPizzas');
+const pizzasDisplay = document.getElementById('pizzasDisplay');
+const imgArray = [
+  'https://cdn-images-1.medium.com/max/1600/1*Wlx63hI3q70lKaGtVvH-ZA.jpeg',
+  'https: //scontent-lht6-1.cdninstagram.com/vp/1e2f35e87ddcf6cf620644ce875efaba/5CD08642/t51.2885-15/e35/49579484_417943578746850_6056746193828834487_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com',
+  'https: //scontent-lht6-1.cdninstagram.com/vp/9155de84cbfbf029358b4632e49d6e47/5CC2A75C/t51.2885-15/e35/47693617_780997805598486_8832491888258135323_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com',
+  'https: //scontent-lht6-1.cdninstagram.com/vp/09a30c8034b15469e5feda41b1a66207/5CC76452/t51.2885-15/e35/49933800_278560039502221_3707768412519390180_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com',
+  'https: //scontent-lht6-1.cdninstagram.com/vp/06c8a405d4da45722a71a23f06182dd3/5CC6E04B/t51.2885-15/fr/e15/s1080x1080/47692966_344489469473010_5988587681001039587_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com',
+]
+
+
+
+function getPhotos() {
+// create a dumpster-node for the images to reside in 
+const fragment = document.createDocumentFragment();
+
+// iterate the data and create <img> elements
+imgArray.forEach(url => {
+  let image = new Image()
+  image.src = url;
+  fragment.appendChild(image);
+})
+
+// dump the fragment into the DOM all the once (FTW)
+document.body.appendChild(fragment);
+}
+
+
+// function getText() {
+
+//   fetch('./data/rawData.txt')
+//     .then((res) => res.text())
+//     .then((data) => {
+//       pizzasDisplay.innerHTML = data;
+//     });
+// }
+
+
+
+getPizzasBtn.addEventListener('click', getPhotos);
